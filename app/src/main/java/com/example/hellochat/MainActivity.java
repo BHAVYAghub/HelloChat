c229200fef576adf200322e3f43bd1dc9c5b3e63
BHAVYA <bhavyakrk@gmail.com>
2019-09-26 20:42:01 +0530

    Test 3

--- app/src/main/java/com/example/hellochat/MainActivity.java
+++ app/src/main/java/com/example/hellochat/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
@@ -14,6 +15,11 @@ import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
@@ -22,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private TabsAccessorAdapter myTabsAccessorAdapter;
     private FirebaseUser currentUser;
     private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth=FirebaseAuth.getInstance();
         currentUser=mAuth.getCurrentUser();
+        RootRef= FirebaseDatabase.getInstance().getReference();
         mToolbar=(Toolbar)findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("HelloChat");
@@ -55,11 +63,45 @@ public class MainActivity extends AppCompatActivity {
         {
             SendUserToLoginActivity();
         }
+        else
+        {
+            VerifyUserExistence();
+        }
+    }
+
+    private void VerifyUserExistence()
+    {
+        String currentUserId=mAuth.getCurrentUser().getUid();
+
+        RootRef.child("Users").child(currentUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if((dataSnapshot.child("name").exists()))
+                {
+                    Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    SendUserToSettingsActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
     }
 
     private void SendUserToLoginActivity() {
         Intent loginIntent=new Intent(MainActivity.this,LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
         startActivity(loginIntent);
+        finish();
     }
 
 
@@ -79,8 +121,16 @@ public class MainActivity extends AppCompatActivity {
              SendUserToLoginActivity();
 
          }
+        if(item.getItemId()==R.id.main_logout_options )
+        {
+            mAuth.signOut();
+            SendUserToLoginActivity();
+
+        }
+
         if(item.getItemId()==R.id.main_settings_options )
         {
+            SendUserToSettingsActivity();
 
         }
         if(item.getItemId()==R.id.main_find_friends_options)
@@ -89,6 +139,14 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
 
+
+    }
+    private void SendUserToSettingsActivity() {
+        Intent settingsIntent=new Intent(MainActivity.this,SettingsActivity.class);
+        settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        startActivity(settingsIntent);
+        finish();
 
     }
 }
