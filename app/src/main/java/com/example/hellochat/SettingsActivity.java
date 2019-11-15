178dc79ed4bc2e2e687ddd8545266c43e8f79ab3
BHAVYA <bhavyakrk@gmail.com>
2019-11-15 22:13:04 +0530

    Test 6

--- app/src/main/java/com/example/hellochat/SettingsActivity.java
+++ app/src/main/java/com/example/hellochat/SettingsActivity.java
@@ -1,9 +1,13 @@
 package com.example.hellochat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -12,6 +16,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +24,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
 
@@ -31,6 +42,10 @@ public class SettingsActivity extends AppCompatActivity {
     private String currentuserID;
     private FirebaseAuth mAuth;
     private DatabaseReference RootRef;
+    private static final int galleryPick=1;
+    private StorageReference UserProfileImagesRef;
+    private ProgressDialog loadingBar;
+    private Toolbar SettingsToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +55,7 @@ public class SettingsActivity extends AppCompatActivity {
         mAuth=FirebaseAuth.getInstance();
         currentuserID=mAuth.getCurrentUser().getUid();
         RootRef= FirebaseDatabase.getInstance().getReference();
+        UserProfileImagesRef= FirebaseStorage.getInstance().getReference().child("Profile Images");
         InitializeFields();
 
         userName.setVisibility(View.INVISIBLE);
@@ -50,6 +66,17 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
        RetrieveUserInfo();
+       userProfileImage.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent galleryIntent=new Intent();
+               galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+               galleryIntent.setType("image/*");
+               startActivityForResult(galleryIntent,galleryPick);
+
+
+           }
+       });
     }
 
 
@@ -67,11 +94,11 @@ public class SettingsActivity extends AppCompatActivity {
         }
         else
         {
-            HashMap<String ,String> profileMap=new HashMap<>();
+            HashMap<String ,Object> profileMap=new HashMap<>();
             profileMap.put("uid",currentuserID);
             profileMap.put("name",setUserName);
             profileMap.put("status",setStatus);
-            RootRef.child("Users").child(currentuserID).setValue(profileMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            RootRef.child("Users").child(currentuserID).updateChildren(profileMap).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
                     if(task.isSuccessful())
@@ -102,6 +129,7 @@ public class SettingsActivity extends AppCompatActivity {
                     String retrieveProfileImage =dataSnapshot.child("image").getValue().toString();
                     userName.setText(retrieveUserName);
                     userStatus.setText(retrieveStatus);
+                    Picasso.get().load(retrieveProfileImage).into(userProfileImage);
 
 
                 }
@@ -132,13 +160,99 @@ public class SettingsActivity extends AppCompatActivity {
 
     }
 
+
+
     private void InitializeFields() {
         UpdateAccountSettings= (Button)findViewById(R.id.update_settings_button);
         userName=(EditText) findViewById(R.id.set_user_name);
         userStatus=(EditText) findViewById(R.id.set_profile_status);
         userProfileImage=(CircleImageView)findViewById(R.id.set_profile_image);
+        loadingBar=new ProgressDialog(this);
+
+
+
+        SettingsToolBar = (Toolbar) findViewById(R.id.settings_toolbar);
+        setSupportActionBar(SettingsToolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setTitle("Account Settings");
 
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==galleryPick&&resultCode==RESULT_OK&&data!=null)
+        {
+            Uri imageUri=data.getData();
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1,1)
+                    .start(this);
+
+
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                loadingBar.setTitle("Setting Profile Image");
+                loadingBar.setMessage("Please Wait");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+                Uri resultUri = result.getUri();
+                final StorageReference filePath=UserProfileImagesRef.child(currentuserID +".jpg");
+                filePath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful())
+                        {
+
+                            Toast.makeText(SettingsActivity.this, "Profile Image Uploaded Successfully", Toast.LENGTH_SHORT).show();
+                            //final String downloadUrl=task.getResult().getDownloadUrl().toString();
+                            filePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    final String downloadUrl=uri.toString();
+                                  //  Toast.makeText(SettingsActivity.this,downloadUrl , Toast.LENGTH_SHORT).show();
+                                    RootRef.child("Users").child(currentuserID).child("image")
+                                            .setValue(downloadUrl).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful())
+                                            {
+                                                Toast.makeText(SettingsActivity.this, "image saved in database successfully", Toast.LENGTH_SHORT).show();
+                                                loadingBar.dismiss();
+                                            }
+                                            else
+                                            {
+                                                String message=task.getException().toString();
+                                                Toast.makeText(SettingsActivity.this, "Error"+message, Toast.LENGTH_SHORT).show();
+                                                loadingBar.dismiss();
+                                            }
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                        else
+                        {
+                            String message=task.getException().toString();
+                            Toast.makeText(SettingsActivity.this, "Erroe"+message, Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+                        }
+                    }
+                });
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+
+
+
+
+    }
+
     private void SendUserToMainActivity() {
         Intent mainIntent=new Intent(SettingsActivity.this,MainActivity.class);
         mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
